11e4448ac61899425780d0804d6402aed11ff8d7
Jerry-jy <1571849142@qq.com>
2023-02-13 16:42:05 +0800

    test1

---
+++ src/main/java/com/jerry/service/impl/ProductTypeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.jerry.service.impl;
+
+import com.jerry.pojo.ProductType;
+import com.jerry.service.ProductTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * ClassName: ProductTypeServiceImpl
+ * Package: com.jerry.service.impl
+ * Description:
+ *
+ * @Author jerry_jy
+ * @Create 2023-02-13 16:40
+ * @Version 1.0
+ */
+@Service
+public class ProductTypeServiceImpl implements ProductTypeService {
+    @Override
+    public List<ProductType> getAll() {
+        return null;
+    }
+}
